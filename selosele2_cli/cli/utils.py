53b1053ec7692de431bee53f167084669cb9c3ec
selosele <osh3384@naver.com>
2023-12-29 13:20:26 +0900

    디렉터리 구조 개선

--- selosele2_cli/cli/utils.py
+++ selosele2_cli/cli/utils.py
@@ -2,6 +2,6 @@ import click
 
 def text(text: str, color: str = '') -> str:
   """
-  click.style 텍스트와 색상을 완성하여 반환
+  click.style 텍스트와 색상을 완성하여 반환한다.
   """
   return click.style(text, fg=color)
--- selosele2_cli/main.py
+++ selosele2_cli/main.py
@@ -1,4 +1,37 @@
-from selosele2_cli.cli import main
+import click
+from simple_term_menu import TerminalMenu
+import selosele2_cli.cli.utils as utils
+import selosele2_cli.cli.menu as menu
+import selosele2_cli.cli.signin as signin
+import selosele2_cli.cli.blog_config as blog_config
+
+@click.command()
+def main():
+  menu_exited = False
+    
+  while not menu_exited:
+    menu.print_main_title()
+    
+    click.echo('=====================================')
+    click.echo('메뉴 선택')
+    click.echo('=====================================')
+  
+    # 메뉴 선택
+    options = ['로그인', '환경설정', '프로그램 종료']
+    terminal_menu = TerminalMenu(options)
+    menu_entry_index = terminal_menu.show()
+    
+    # 로그인
+    if menu_entry_index == 0:
+      signin.main()
+    
+    # 환경설정
+    if menu_entry_index == 1:
+      blog_config.main()
+    
+    # 프로그램 종료
+    if menu_entry_index == 2:
+      menu_exited = True
 
 if __name__ == '__main__':
   main()
